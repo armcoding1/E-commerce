00d5da12e1e82eda570695e79c1c05b3c296b3d3
armcoding1 <armenbaghdasaryancoding@gmail.com>
2024-04-17 01:12:00 -0700

    edited layouts

--- admin/app.js
+++ admin/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
+app.use(express.static("public"));
 app.use("/", indexRouter);
 
 module.exports = app;
