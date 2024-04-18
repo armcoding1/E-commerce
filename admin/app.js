a9fe4fa97db480e5f1a89e4010cce03f3d77ed9b
armcoding1 <armenbaghdasaryancoding@gmail.com>
2024-04-17 23:38:10 -0700

    Modified home page

--- admin/app.js
+++ admin/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.use(express.static("public"));
+app.use(morgan("dev"));
 app.use("/", indexRouter);
 
 module.exports = app;
