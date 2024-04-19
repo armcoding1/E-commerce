fbb478183aa66818599895d503c9c8f3b6060f1b
armcoding1 <armenbaghdasaryancoding@gmail.com>
2024-04-18 23:37:01 -0700

    h

--- admin/app.js
+++ admin/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.use(express.static("public"));
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use("/", indexRouter);
 
 module.exports = app;
