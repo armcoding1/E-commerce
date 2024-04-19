b123e3030333323c157b4cf9e316219f136bea6f
armcoding1 <armenbaghdasaryancoding@gmail.com>
2024-04-19 00:49:02 -0700

    Some changes in layout

--- admin/server.js
+++ admin/server.js
@@ -3,11 +3,11 @@ const app = require("./app");
 require("dotenv").config();
 
 async function startServer() {
-    const DB_URL = process.env.DB_URL;
+    // const DB_URL = process.env.DB_URL;
     const PORT = process.env.PORT;
 
-    await connect(DB_URL);
-    console.log("DB is successfuly connected");
+    // await connect(DB_URL);
+    // console.log("DB is successfuly connected");
 
     app.listen(PORT, () => {
         console.log(`Listening on http://localhost:${PORT}`);
